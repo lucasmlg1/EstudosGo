bdce8142f1639328e6774adea8da670265eaf93f
lucasmlg1 <lucasmlg@live.com>
2026-07-20 14:25:18 -0300

    add: primeiro codigo em Go

---
+++ primeiroprograma/primeiro.go
@@ -0,0 +1,9 @@
+package main // Precisamos referenciar que esse arquivo está dentro do pacote main para o GO identificar que o arquivo é exec.
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Olá, mundo!")
+}
