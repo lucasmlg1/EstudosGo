3b16385389c082cbac2dcfc49b59ec8d22d97e67
lucasmlg1 <lucasmlg@live.com>
2026-07-30 14:05:12 -0300

    Criação função para testes

---
+++ 19 - Testes automatizados/01 - Introducao/enderecos/enderecos.go
@@ -0,0 +1,21 @@
+package enderecos
+
+import "strings"
+
+//TipoEndereco verifica se tem um endereco valido e o retorna
+func TipoEndereco(endereco string) string {
+	tiposValidos := []string{"rua", "avenida", "estrada", "rodovia"}
+	enderecoComLetrasMinuscula := strings.ToLower(endereco)
+	primeiraPalavraDoEndereco := strings.Split(enderecoComLetrasMinuscula, " ")[0]
+
+	enderecoValido := false
+	for _, tipo := range tiposValidos {
+		if primeiraPalavraDoEndereco == tipo {
+			enderecoValido = true
+		}
+	}
+	if enderecoValido {
+		return primeiraPalavraDoEndereco
+	}
+	return "Tipo inválido de endereco."
+}
