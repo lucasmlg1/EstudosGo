0328bc431a9c315f1a2abaa2935c224a1db869de
lucasmlg1 <lucasmlg@live.com>
2026-07-31 13:08:13 -0300

    Casos com mais testes

--- 19 - Testes automatizados/01 - Introducao/enderecos/enderecos_test.go
+++ 19 - Testes automatizados/01 - Introducao/enderecos/enderecos_test.go
@@ -2,15 +2,33 @@
 
 package enderecos
 
-import "testing"
+import (
+	"testing"
+)
+
+type cenarioTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
 
 func TestTipoDeEndereco(t *testing.T) {
-	// Começa com TestXxxxXxxxX o nome da função obrigatoriamente.
-	enderecoParaTeste := "Rua ABC"
-	tipoDeEnderecoEsperado := "Rua"
-	tipoDeEnderecoRecebido := TipoEndereco(enderecoParaTeste)
-	if tipoDeEnderecoRecebido != tipoDeEnderecoEsperado {
-		t.Error("O tipo é diferente do esperado!")
+	// Começa com TestXxxxXxxxX o nome da função obrigatoriamente
+
+	cenariosDeTeste := []cenarioTeste{
+		{"Avenida ABC", "Avenida"},
+		{"Rua das rosas", "Rua"},
+		{"Rodovia dos imigrantes", "Rodovia"},
+		{"Praça das rosas", "Tipo inválido de endereco."},
+		{"Estrada qualquer", "Estrada"},
+		{"RUA DOS BOBOS", "Rua"},
+		{"", "Tipo inválido de endereco."},
+	}
+
+	for _, cenario := range cenariosDeTeste {
+		tipoEnderecoRecebido := TipoEndereco(cenario.enderecoInserido)
+		if tipoEnderecoRecebido != cenario.retornoEsperado {
+			t.Error("O tipo é diferente do esperado!")
+		}
 	}
 
 }
